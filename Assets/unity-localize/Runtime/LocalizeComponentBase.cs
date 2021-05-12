ac6706494ae5ed9a3ee90ccf361079c270e1a97a
strix <korstrix@gmail.com>
2021-05-13 00:32:08 +0900

    init commit

---
+++ Assets/unity-localize/Runtime/LocalizeComponentBase.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UNKO.Localize
+{
+    public abstract class LocalizeComponentBase : MonoBehaviour
+    {
+        static protected bool s_isAppQuitting = false;
+        static protected ILocalizeManager s_manager;
+
+        [SerializeField]
+        protected string _languageKey;
+
+        [SerializeField]
+        protected List<string> _languageParam = new List<string>();
+
+        public static void Init(ILocalizeManager manager)
+        {
+            s_manager = manager;
+        }
+
+        public void SetLanguageKey(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                Debug.LogWarning($"{name} SetLanguageKey - key is null or empty", this);
+                return;
+            }
+
+            _languageKey = key;
+            _languageParam.Clear();
+            OnChangeLanguage(s_manager.currentLanguage);
+        }
+
+        public void SetLanguageKeyWithParam(string key, params string[] param)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                Debug.LogWarning($"{name} SetLanguageKey - key is null or empty", this);
+                return;
+            }
+
+            _languageKey = key;
+            _languageParam.Clear();
+            _languageParam.AddRange(param);
+            OnChangeLanguage(s_manager.currentLanguage);
+        }
+
+        public void UpdateLocalize()
+        {
+            OnChangeLanguage(s_manager.currentLanguage);
+        }
+
+        void OnEnable()
+        {
+            if (s_manager == null)
+            {
+                Invoke(nameof(Setup), 0.1f);
+                return;
+            }
+
+            Setup();
+        }
+
+        void OnDisable()
+        {
+            if (s_isAppQuitting)
+                return;
+
+            s_manager.OnChangeLanguage -= OnChangeLanguage;
+        }
+
+        private void Setup()
+        {
+            if (s_manager == null)
+            {
+                Debug.LogError($"{name}.Setup - _manager == null", this);
+                return;
+            }
+
+            s_manager.OnChangeLanguage -= OnChangeLanguage;
+            s_manager.OnChangeLanguage += OnChangeLanguage;
+            UpdateLocalize();
+        }
+
+        private void OnApplicationQuit()
+        {
+            s_isAppQuitting = true;
+        }
+
+        protected abstract void OnChangeLanguage(SystemLanguage language);
+    }
+
+#if UNITY_EDITOR
+    public abstract class LocalizeComponentBase_Inspector<T> : Editor
+        where T : LocalizeComponentBase
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            // if (GUILayout.Button("update"))
+            // {
+            //     Debug.Log("update");
+            // }
+        }
+    }
+#endif
+}
---
+++ Assets/unity-localize/Runtime/LocalizeManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UNKO.Localize
+{
+    public class LocalizeManagerComponent : MonoBehaviour, ILocalizeManager
+    {
+        public event System.Action<SystemLanguage> OnChangeLanguage;
+        public event System.Action<Font> OnChangeFont;
+        public SystemLanguage currentLanguage { get; private set; }
+
+        private Dictionary<string, ILocalizeData> _languageDictionary = new Dictionary<string, ILocalizeData>();
+        private Dictionary<SystemLanguage, ILocalizeFontData> _fontDictionary = new Dictionary<SystemLanguage, ILocalizeFontData>();
+
+        public ILocalizeManager AddData(IEnumerable<ILocalizeData> datas)
+        {
+            foreach (ILocalizeData data in datas)
+                _languageDictionary.Add(data.LocalizeID, data);
+
+            return this;
+        }
+
+        public ILocalizeManager AddFontData(IEnumerable<ILocalizeFontData> datas)
+        {
+            foreach (ILocalizeFontData data in datas)
+                _fontDictionary.Add(data.language, data);
+
+            return this;
+        }
+
+        public ILocalizeManager ChangeLanguage(SystemLanguage language)
+        {
+            currentLanguage = language;
+            OnChangeLanguage?.Invoke(language);
+            if (_fontDictionary.TryGetValue(language, out ILocalizeFontData fontData))
+            {
+                Font font = fontData.GetFont();
+                if (font == null)
+                {
+                    Debug.LogError($"{name}.{nameof(ChangeLanguage)}(language({language}) font == null");
+                    return this;
+                }
+
+                OnChangeFont?.Invoke(font);
+            }
+
+            return this;
+        }
+
+        public string GetLocalizeText(string languageID, params string[] param)
+        {
+            if (string.IsNullOrEmpty(languageID))
+                return "";
+
+            if (_languageDictionary.TryGetValue(languageID, out var data) == false)
+            {
+                Debug.LogError($"LanguageManager GetLocalizeText('{languageID}') == not found data");
+#if UNITY_EDITOR
+                return $"Error:'{languageID}'";
+#else
+                return "";
+#endif
+            }
+
+            string text = data.GetLocalizeText(currentLanguage);
+            if (param.Length > 0)
+                text = string.Format(text, param);
+
+            return text;
+        }
+
+        public bool IsValidID(string languageID)
+        {
+            return _languageDictionary.ContainsKey(languageID);
+        }
+    }
+}
